95fbcba1b84138e9336b30d50ee97346bbf5e9aa
junaidbinjaman <me@junaidbinjaman.com>
2024-08-24 10:00:00 +0600

    The navigations are added

--- components/MealDetails.js
+++ components/MealDetails.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import {React} from 'react';
 import {View, StyleSheet, Text} from 'react-native';
 
-const MealDetails = ({duration, complexity, affordability}) => {
+const MealDetails = ({duration, complexity, affordability, style, textStyle}) => {
     return (
-        <View>
-            <Text style={styles.detailItem}>{duration}m</Text>
-            <Text style={styles.detailItem}>{complexity.toUpperCase()}</Text>
-            <Text style={styles.detailItem}>{affordability.toUpperCase()}</Text>
+        <View style={[styles.details, style]}>
+            <Text style={[styles.detailItem, textStyle]}>{duration}m</Text>
+            <Text style={[styles.detailItem, textStyle]}>{complexity.toUpperCase()}</Text>
+            <Text style={[styles.detailItem, textStyle]}>{affordability.toUpperCase()}</Text>
         </View>
     );
 };
--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,40 +1,86 @@
-import React from 'react';
-import {View, Text, Image, StyleSheet} from 'react-native';
+import {React, useLayoutEffect} from 'react';
+import {View, Text, Image, StyleSheet, ScrollView, Button} from 'react-native';
 import {MEALS} from '../data/dummy-data';
 import MealDetails from '../components/MealDetails';
+import Subtitle from '../components/MealDetail/Subtitle';
+import List from '../components/MealDetail/List';
+import IconButton from '../components/IconButton';
 
-const MealDetailScreen = ({route}) => {
+const MealDetailScreen = ({route, navigation}) => {
     const mealId = route.params.mealId;
+
+    function headerButtonPressHandler() {
+        console.log('Clicked');
+    }
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerRight: () => {
+                return (
+                    <IconButton
+                        icon='star'
+                        color='white'
+                        onPress={headerButtonPressHandler}
+                    />
+                );
+            },
+        });
+    }, [navigation, headerButtonPressHandler]);
 
     const selectedMeal = MEALS.find((meal) => meal.id === mealId);
     return (
-        <View>
+        <ScrollView style={styles.rootContainer}>
             <Image source={{uri: selectedMeal.imageUrl}} style={styles.image} />
-            <Text>{selectedMeal.title}</Text>
+            <Text style={styles.title}>{selectedMeal.title}</Text>
 
             <MealDetails
                 duration={selectedMeal.duration}
                 complexity={selectedMeal.complexity}
                 affordability={selectedMeal.affordability}
+                textStyle={styles.detailText}
             />
 
-            <Text>Ingredients</Text>
-            {selectedMeal.ingredients.map((ingredient) => (
-                <Text key={ingredient}>{ingredient}</Text>
-            ))}
-            <Text>Steps</Text>
-            {selectedMeal.steps.map((step) => (
-                <Text key={step}>{step}</Text>
-            ))}
-        </View>
+            <View style={styles.listOuterContainer}>
+                <View style={styles.listContainer}>
+                    <Subtitle>Ingredients</Subtitle>
+                    <List data={selectedMeal.ingredients} />
+                    <Subtitle>Steps</Subtitle>
+                    <List data={selectedMeal.steps} />
+                </View>
+            </View>
+        </ScrollView>
     );
 };
 
 export default MealDetailScreen;
 
 const styles = StyleSheet.create({
+    rootContainer: {
+        marginBottom: 32,
+    },
+
     image: {
-        width: 200,
-        height: 100
-    }
+        width: '100%',
+        height: 350,
+    },
+
+    title: {
+        fontWeight: 'bold',
+        fontSize: 24,
+        margin: 8,
+        textAlign: 'center',
+        color: 'white',
+    },
+
+    detailText: {
+        color: 'white',
+    },
+
+    listOuterContainer: {
+        alignItems: 'center',
+    },
+
+    listContainer: {
+        width: '80%',
+    },
 });
